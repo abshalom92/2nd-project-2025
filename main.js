efcf18353ef1729d7351d88140be6c2908078b48
abshalom92 <abshalom92@gmail.com>
2025-08-23 01:14:27 -0400

    added more logic in the JS along with comments about where I would like to go with the webpage.

--- main.js
+++ main.js
@@ -11,15 +11,26 @@ const images = [
 function addImage(numb) {
     const img = document.createElement("img");
     img.src = images[numb];
-    console.log(images[numb]);
+    document.getElementById("opponent").appendChild(img);
     //figure out how to get alt image to match
     
 }
 
 function startGame() {
-    const difficulty = document.getElementById('difficulty');
-     
+    // const playBoxDiv = document.getElementById("play-box");
+    // const difficulty = document.createElement('h3');
+    // playBoxDiv.appendChild(difficulty);
+      
+    const playButton = document.getElementById("play-button");
+    const showPlayButton = playButton.style;
+    const showDifficulty = document.getElementsByName("label");
+    console.log(showDifficulty);
+    showDifficulty.style.display = "block";
+    showPlayButton.display = "none";
+    
+    // chooseDifficulty.
+    // const imageNumber = 
     // const selectDifficulty = 
     //maybe a radio button? will fifgure out tomorrow
-    addImage(gameDifficulty);
+    // addImage();
 }
