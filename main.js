2b2b3f9756e35af24430d924f188fbd035cb5e92
abshalom92 <abshalom92@gmail.com>
2025-08-29 00:38:41 -0400

    worked on rps win logic. left in some deprecated code. chatgpt gave me the answer, but I want to try again tomorrow when I am fresh

--- main.js
+++ main.js
@@ -54,12 +54,6 @@ function startGame() {
     document.getElementById("play-button").style.display = "none";
     document.getElementById("select-difficulty").style.display = "block";
     document.getElementById("battleground").style.display = "none";
-    
-    
-    // chooseDifficulty.
-    // const imageNumber = 
-    // const selectDifficulty = 
-    // addImage();
 }
 
 function acceptOpponent() {
@@ -73,10 +67,29 @@ function acceptOpponent() {
 function playRPS () {
     document.getElementById("battleground").style.display = "flex";
     document.getElementById("game-container").style.display = "none";
+    
     const opponentDiv = document.getElementById("opponents-choice");
     opponentDiv.src = playOptions[Math.floor(3*Math.random())];
-    //next prob to solve after 8/27 is to set up the game logic. 
-    }
+    const opponentPlay = opponentDiv.src;
+    const yourPlay = document.getElementById("your-play").src;
+    const youWonRock = yourPlay === playOptions[0] && opponentPlay ===playOptions[2];
+    const youWonPaper = yourPlay === playOptions[1] && opponentPlay ===playOptions[0];
+    const youWonScissors = yourPlay === playOptions[2] && opponentPlay ===playOptions[1];
+    const tieGame = yourPlay === opponentPlay;
+
+    const youWin = youWonRock || youWonPaper || youWonScissors;
+
+    console.log(yourPlay, playOptions[0]);
+        console.log(opponentPlay);
+    // if (tieGame) {
+    //   console.log('Wow, you tied');
+    // } else if (youWin){
+    //   console.log("You win!!");
+    // } else {
+    //   console.log("Sorry, you lost.")
+    // }
+
+    } 
 
 
 //     const userPlay = document.getElementById("your-play").value;
